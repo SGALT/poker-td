6d7584a6bd74055d5e1e2179090f87a75e2aefae
Sylvain Galtier <sylvaingaltier@gmail.com>
2019-07-03 09:33:27 +0200

    links on navbar ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def infos
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'infos', to: "pages#infos", as: :infos
 
   resources :tournaments do
     post "busted"
